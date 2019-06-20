4b22a6a42fd83d0d9b8ad4409538cbb36e465066
williamsoftwarecode <williamsoftwarecode@gmail.com>
2019-06-20 20:58:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# MEAN Stack Template
+
+This is a Issue Tracker created based on the following websites: 
+
+https://medium.com/codingthesmartway-com-blog/angular-6-mean-stack-crash-course-part-1-front-end-project-setup-and-routing-89bec8332cea
+https://medium.com/codingthesmartway-com-blog/angular-6-mean-stack-crash-course-part-2-implementing-the-back-end-642567e16bb9
+https://medium.com/codingthesmartway-com-blog/angular-6-mean-stack-crash-course-part-3-connecting-front-end-to-back-end-760e26e2c74d
+https://medium.com/codingthesmartway-com-blog/angular-6-mean-stack-crash-course-part-4-completing-the-user-interface-94f2d4da3490
+
+https://medium.com/@hellotunmbi/how-to-deploy-angular-application-to-heroku-1d56e09c5147
+
+It is ready for deployment on Heroku through Git. 
+
+## Configuration
+
+Two files need to be configured before use: 
+1. server.js - This app assumes MongoDB will be used as the default database. Configure the connection URI to the selected database. 
+2. issue.service.ts - Configure the URI to point to the website. 
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MatToolbarModule, 
+  MatFormFieldModule, 
+  MatInputModule, 
+  MatOptionModule, 
+  MatSelectModule, 
+  MatIconModule, 
+  MatButtonModule, 
+  MatCardModule, 
+  MatTableModule, 
+  MatDividerModule, 
+  MatSnackBarModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './components/list/list.component';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
+import { IssueService } from './issue.service';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateComponent,
+    EditComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTableModule,
+    MatDividerModule,
+    MatSnackBarModule
+  ],
+  providers: [IssueService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
